f221586242678d20fb875eae0255f11665d9a4e7
Simon Duck <its.duck@gmail.com>
2025-08-06 12:34:44 +0100

    clickable map added to form

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38cebbf3524f2b56ccce21fb6bac031cd1515ec6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e736cb59e0b8e3e8c4e94b5dcf8946a6a32404d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ui/src/components/FormComponents/FindDataForNearestStationForm.tsx
+++ ui/src/components/FormComponents/FindDataForNearestStationForm.tsx
@@ -1,7 +1,10 @@
-import { JSX, useEffect, useState } from "react";
+import { type JSX, useState } from "react";
 import { useForm } from "react-hook-form";
 import "./FindDataForNearestStationForm.css";
-  
+import { LocationMarkerMap } from "./LocationMarkerMap";
+import { MapContainer, TileLayer } from "react-leaflet";
+import { latLng, LatLng } from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 export function FindDataForNearestStationForm(): JSX.Element {
   interface FormData {
@@ -9,33 +12,85 @@ export function FindDataForNearestStationForm(): JSX.Element {
     Latitude: number;
   }
 
- const { register, handleSubmit, formState: { errors } } = useForm<FormData>({defaultValues:{Longitude: 0, Latitude: 0}});
-
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>({ defaultValues: { Longitude: 0, Latitude: 0 } });
   const onSubmit = (data: FormData): void => console.log(data);
   console.log(errors);
-  
+  const [position, setPosition] = useState<[number, number]>([0, 0]);
+
   return (
+    <div>
+     <div className="map-container">
+        <MapContainer
+          center={{ lat: 51.553124, lng: -0.142594 }}
+          maxBounds={[
+            [-90, -180],
+            [90, 180],
+          ]}
+          maxBoundsViscosity={1}
+          minZoom={2.5
+            
+          }
+          zoom={1}
+          scrollWheelZoom={true}>
+          <TileLayer
+          noWrap={true}
+          bounds={[[-90, -180],
+            [90, 180]]}
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />{" "}
+          <LocationMarkerMap
+            position={latLng(position[0], position[1])}
+            setPosition={(pos: LatLng) => setPosition([pos.lat, pos.lng])}
+            setValue={setValue}
+          />
+        </MapContainer>
+      </div>
     <div className="container">
       
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <p className="font-medium">Click the Map to get results for nearest Air Quality Station</p>
-      <div className= "flex flex-row justify-center">
-      <div className="flex flex-col justify-center self-center">
-      <p className="">Longitude</p>
-      <input className="font-medium align-items-center" type="text" placeholder="Longitude" {...register("Longitude", {required: true, pattern: /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)/i})} />
-      </div>
-      <div className="flex flex-col justify-self-center">
-        <p className=" m-1px">
-        Latitude
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <p className="font-medium">
+          Click Map to select location
         </p>
-      <input className="font-medium" type="text" placeholder="Latitude" {...register("Latitude", {required: true, pattern: /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)/i})} />
-      </div>
-      </div>
-      <p>
-      <input className="font-medium" type="submit" />
-      </p>
-      
-    </form>
+
+        <div></div>
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col justify-center self-center">
+            <p className="">Longitude</p>
+            <input
+              className="font-medium align-items-center"
+              type="text"
+              placeholder="Longitude"
+              {...register("Longitude", {
+                required: true,
+                pattern: /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)/i,
+              })}
+            />
+          </div>
+          <div className="flex flex-col justify-self-center">
+            <p className=" m-1px">Latitude</p>
+            <input
+              className="font-medium"
+              type="text"
+              placeholder="Latitude"
+              {...register("Latitude", {
+                required: true,
+                pattern: /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)/i,
+              })}
+            />
+          </div>
+        </div>
+        <p>
+          <input className="font-medium" type="submit" />
+        </p>
+      </form>
+     
+    </div>
     </div>
   );
 }
