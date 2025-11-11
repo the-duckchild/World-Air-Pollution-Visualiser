60c003b9cf509a9a3147d640f2a93beb0d4bba53
Simon Duck <its.duck@gmail.com>
2025-11-11 21:27:30 +0000

    rate limiting registered in Program.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,4 +1,5 @@
 using api.Repositories;
+using api.Middleware;
 using dotenv.net;
 
 var AllowSpecificOrigins = "_AllowSpecificOrigins";
@@ -47,6 +48,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseRateLimiting();
 
 app.UseCors(AllowSpecificOrigins);
 
