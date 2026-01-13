9930d5ae23e00077f73e4239cb1058b450b26331
Simon Duck <its.duck@gmail.com>
2026-01-13 10:44:26 +0000

    current state

--- api.tests/obj/Debug/net8.0/api.tests.AssemblyInfo.cs
+++ api.tests/obj/Debug/net8.0/api.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f551b61508d43d312010fa79b16f3b9bcd4c132")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d01eb4fd1e02ce1ca8c9b93017d000b27dd241")]
 [assembly: System.Reflection.AssemblyProductAttribute("api.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
